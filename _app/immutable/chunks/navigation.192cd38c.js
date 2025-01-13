207d36d585162b12afd94c3f437bb49c3cee799e
lisaviktorsson <82220848+lisaviktorsson@users.noreply.github.com>
2025-01-13 14:51:05 +0000

    Deploying to gh-pages from @ lisaviktorsson/webdev_two@e720cf26e514335a21063e395ff0e5f77e22f631 🚀

---
+++ _app/immutable/chunks/navigation.192cd38c.js
@@ -0,0 +1,1 @@
+import{k as o}from"./singletons.62b2871f.js";const a=o("goto"),i=o("invalidate_all");export{a as g,i};
--- _app/immutable/chunks/navigation.9186c7ac.js
+++
@@ -1,1 +0,0 @@
-import{k as o}from"./singletons.d6caf623.js";const a=o("goto"),i=o("invalidate_all");export{a as g,i};
